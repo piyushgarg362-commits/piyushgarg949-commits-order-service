e41fa31a264efc497d9aaafda72be31405a71d9c
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-09-09 22:15:35 -0400

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const orderRoutes = require("./routes/orderRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({
+    service: "order-service",
+    status: "UP"
+  });
+});
+
+app.use("/orders", orderRoutes);
+
+module.exports = app;
